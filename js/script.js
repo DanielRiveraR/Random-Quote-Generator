1d442401e20f12f4507da34c2d7dab57bf656b99
DanielRiveraR <dani_wooten@hotmail.com>
2021-09-13 00:47:53 -0700

    Added requirements for exceeds grade.

--- js/script.js
+++ js/script.js
@@ -3,60 +3,59 @@ Treehouse Techdegree:
 Project 4 - Random Quote Generator
 *************************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community
 
 /*** 
- * `quotes` array 
+ * An object stored in an array as a list of quotes, authors and, in some cases, citations and year.  
 ***/
 const quotes = [
   { 
     quote: "Who controls the past controls the future. Who controls the present controls the past.",
     source: "George Orwell",
     citation: "Nineteen Eighty-Four",
-    year: 1947
+    year: 1947,
+    category: "Inspiring"
   },
   { 
     quote: "I am not afraid of storms, for I am learning how to sail my ship.",
     source: "Louisa May Alcott",
     citation: "Little Women",
-    year: 1869
+    category: "Inspiring"
   },
   { 
     quote: "This above all: To thine own self be true, And it must follow, as the night the day, Thou canst not then be false to any man",
     source: "Shakespeare",
-    
-    
-
+    category: "Inspiring"
   },
   { 
     quote: "The only way out of the labyrinth of suffering is to forgive.",
     source: "John Green",
     citation: "Looking for Alaska",
-    year: 2005
-
+    year: 2005,
+    category: "Wisdom"
   },
   { 
     quote: "I wanted you to see what real courage is, instead of getting the idea that courage is a man with a gun in his hand. It’s when you know you’re licked before you begin but you begin anyway and you see it through no matter what. You rarely win, but sometimes you do.",
     source: "Harper Lee",
     citation: "To Kill a Mockingbird",
-    year: 1960
+    year: 1960,
+    category: "Wisdom"
   }
 ];
 
 
-
 /***
- * `getRandomQuote` function
+ * This function takes out randomly a quote from the array above.
 ***/
 function getRandomQuote() {
   const randomNum = Math.floor(Math.random() * quotes.length);
   return quotes[randomNum];
 }
 
+
 /***
- * `printQuote` function
+ * This function perform this three task: (a) picks a random quote object from the array.
+ * (b) Use the returned quote object to build a string of HTML and quote properties.
+ * (c) Use that string to display a random quote in the browser.
 ***/
 function printQuote() {
   const quoteBrowser = getRandomQuote();
@@ -72,18 +71,42 @@ function printQuote() {
       const yearHTML = `<span class="year"> ${quoteBrowser.year}</span>`;
       html = html.concat(yearHTML);
   }
+  if (quoteBrowser.category) {
+    const categoryHTML = `<span class="year"> ${quoteBrowser.category}</span>`;
+    html = html.concat(categoryHTML);
+  }
   html = html.concat(`</p>`);
   return document.getElementById('quote-box').innerHTML = html; 
 }
 
-
- 
-
+/*** 
+ * This function updates the background color to a random color.
+***/
+function randomBgColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const backgroundColor = `rgb( ${r}, ${g}, ${b} )`;
+  document.body.style.background =backgroundColor;
+}
 
 
 /***
  * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", randomBgColor, false);
+
+/***
+ * This function update the quote on the page at regular intervals of 10 secs.
+ ***/
+setInterval(() => {
+  printQuote();
+  randomBgColor();
+}, 10000);
+  
+  
+
+
+
